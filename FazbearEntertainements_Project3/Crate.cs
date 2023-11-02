b894119359d2c6c64aa77554082a46db941b9eb8
ConeDome <elic2003@icloud.com>
2023-11-02 13:48:57 -0400

    I made the Crate class with it's id and price

---
+++ FazbearEntertainements_Project3/Crate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Crate
+{
+
+	string _id { get; set; }
+	double _price { get; set; }
+
+    public Crate(string id, double price)
+	{
+		_id = id;
+		_price = price;
+	}
+}
