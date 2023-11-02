1270b95f7e5dc71a21da22ba78d8d621871cae6b
ShadowWlkr <zackj1205513@gmail.com>
2023-11-02 13:55:06 -0400

    i made a dock

---
+++ FazbearEntertainements_Project3/Dock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FazbearEntertainements_Project3
+{
+    internal class Dock
+    {
+        public string Id {get; private set;}
+        public Queue<Truck> Line { get; private set;}
+        public double TotalSales { get; private set;}
+        public int TotalCrates {get; private set;}
+        public int TotalTrucks { get; private set;}
+        public int TimeInUse {get; private set;}
+        public int TimeNotInUse {get; private set;}
+
+        public void JoinLine()
+        {
+
+        }
+
+        public Truck SendOff()
+        {
+
+        }
+    }
+}
