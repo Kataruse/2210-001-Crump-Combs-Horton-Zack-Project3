ea8b4d52a490b2eeffb949e37db4b39c43b1be75
Nicholas Crump <113931857+Kataruse@users.noreply.github.com>
2023-11-03 19:06:53 -0400

    Add files via upload

--- FazbearEntertainements_Project3/Truck.cs
+++ FazbearEntertainements_Project3/Truck.cs
@@ -6,17 +6,36 @@ using System.Threading.Tasks;
 
 namespace FazbearEntertainements_Project3
 {
-    internal class Truck<T>
+    internal class Truck
     {
         public string Driver { get; set; }
         public string DeliveryCompany { get; set; }
-        public Stack<T> Trailer { get; set; }
+        public Stack<Crate> Trailer { get; set; }
 
         public Truck(string driver, string deliveryCompany)
         {
             Driver = driver;
             DeliveryCompany = deliveryCompany;
-            Trailer = new Stack<T>(15);
+            Trailer = new Stack<Crate>(15);
+        }
+        
+        /// <summary>
+        /// Loads a crate starting at the back of the truck.
+        /// </summary>
+        /// <param name="crate">Crate to load into the truck.</param>
+        public void Load(Crate crate)
+        {
+            Trailer.Push(crate);
+        }
+
+        /// <summary>
+        /// Unloads a crate starting from the front of the truck.
+        /// </summary>
+        /// <returns></returns>
+        public Crate Unload()
+        {
+            Crate crateToUnload = Trailer.Pop();
+            return crateToUnload;
         }
     }
 }
